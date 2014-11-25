1fbf65bb5626c7e3b184dd07369d2f4a47be52e1
Sean Smith <swsmith@bu.edu>
2014-11-24 20:00:37 -0500

    Happy Thanksgiving (almost

--- yo/static/js/main.js
+++ yo/static/js/main.js
@@ -8,7 +8,7 @@
     if(getUrlParameter("username") != ""){
         $("#username").val(getUrlParameter("username"));
     }
-    if(getUrlParameter("username") != ""){
+    if(getUrlParameter("name") != ""){
         $("#name").val(getUrlParameter("name"));
     }
 
@@ -49,25 +49,4 @@
             });
             return false;
         });
-        /*
-      $("#yo_form").on("submit", function(event) {
-          event.preventDefault();
-          $("#yo_form").hide();
-          $("#subscribers").hide();
-          username = $("#username").val();
-          url = $("#url").val();
-          if(url==""){
-           url = "http://seanssmith.me";
-            }
-            $.ajax({
-                type: "POST",
-                url: "/yo",
-                data: {"username": username, "url": url},
-                success: function(data){
-                    $("#success").html("<h3 class=\"text-center\">"+data+"</h3>");
-                }
-            });
-            return false;
-        });
-        */
     });
